b0af8d721e7de45683e7faf60cde7028cfd0f1a0
Iris <a17.bnxk@g.chuo-u.ac.jp>
2018-06-08 12:10:50 +0900

    演算子を実装

--- simplex.rb
+++ simplex.rb
@@ -1,9 +1,147 @@
 class Fraction
-  attr_reader :numerator, :denominator
+  attr_accessor :numerator, :denominator, :numer, :denom
 
   def initialize(numerator, denominator)
     @numerator = numerator
     @denominator = denominator
+    @numer = numerator
+    @denom = denominator
   end
 
+  def -@
+    self.numerator = -self.numerator
+    self
+  end
+
+  def +(other)
+    case other
+      when Fraction
+        if self.denominator == other.denominator
+          self.numerator += other.numerator
+        else
+          self.numerator, self.denominator, other = Fraction.complete(self, other)
+          self.numerator += other.numerator
+        end
+        self.reduce
+      when Integer
+        self + other.to_fr 
+      else
+        nil
+    end
+  end
+
+  def -(other)
+    self + (-other)
+  end
+
+  def *(other)
+    case other
+      when Fraction
+        self.numerator *= other.numerator
+        self.denominator *= other.denominator
+        self.reduce
+      when Integer
+        self + other.to_fr 
+      else
+        nil
+    end
+  end
+
+  def /(other)
+    self * other.to_fr.inverse
+  end
+
+  def ==(other)
+    self.numerator == other.numerator and self.denominator == other.denominator
+  end
+
+  alias === ==
+
+  def <=>(other)
+    case other
+      when Fraction
+        if self > other
+          1
+        elsif self == other
+          0
+        elsif self < other
+          -1
+        else
+          nil
+        end
+      when Integer
+        self <=> other.to_fr
+      else
+        nil
+      end
+  end
+
+  def >(other)
+    self.numerator, self.denominator, other = Fraction.complete(self, other)
+    self.numerator > other.numerator
+  end
+
+  def >=(other)
+    self == other or self > other
+  end
+
+  def <(other)
+    self.numerator, self.denominator, other = Fraction.complete(self, other)
+    self.numerator < other.numerator
+  end
+
+  def <=(other)
+    self == other or self < other
+  end
+
+  def **(other)
+    case other
+      when Fraction
+        nil
+      when Integer
+        self.numerator **= other
+       self.reduce 
+      else
+        nil
+    end
+  end
+
+
+  def reduce
+    gcd = self.numerator.gcd(self.denominator)
+    self.numerator /= gcd
+    self.denominator /= gcd
+    self
+  end
+
+  def inverse
+    self.numerator, self.denominator = self.denominator, self.numerator
+    self
+  end
+  
+  def to_fr
+    self
+  end
+
+  def self.complete(a,b)
+    if a.instance_of?(Fraction) and b.instance_of?(Fraction)
+      lcm = a.denominator.lcm(b.denominator)
+      a.numerator *= lcm/a.denominator
+      b.numerator *= lcm/b.denominator
+      a.denominator = lcm
+      b.denominator = lcm
+      return a.numerator, a.denominator, b
+    elsif (a.instance_of?(Fraction) or a.instance_of?(Integer)) and (b.instance_of?(Fraction) or b.instance_of?(Integer))
+      complete(a.to_fr, b.to_fr)
+    end
+  end
+     
+
 end  
+
+class Integer
+  def to_fr
+    Fraction.new(self, 1)
+  end
+end
+
--- test.rb
+++ test.rb
@@ -5,5 +5,153 @@ class Simplextest < Minitest::Test
   def test_new_fraction
     assert Fraction.new(1,1)
   end
+
+  def test_add_fraction_same_denominator
+    f1 = Fraction.new(1,3)
+    f2 = Fraction.new(1,3)
+    f = f1 + f2
+    assert_equal 2, f.numerator
+    assert_equal 3, f.denominator
+  end
+   
+  def test_add_fraction_different_denominator
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    f = f1 + f2
+    assert_equal 5, f.numerator
+    assert_equal 6, f.denominator
+  end
+
+  def test_to_fr
+    f = 3.to_fr
+    assert_equal 3, f.numerator
+    assert_equal 1, f.denominator
+  end
+
+  def test_add_fraction_different_denominator
+    f1 = Fraction.new(1,2)
+    i = 1 
+    f = f1 + i
+    assert_equal 3, f.numerator
+    assert_equal 2, f.denominator
+  end
+
+  def test_inverse_sign
+    f1 = Fraction.new(1,2)
+    f2 = -f1
+    assert_equal -1, f2.numerator
+    assert_equal 2, f2.denominator
+  end
+
+  def test_subtract_fraction
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    f = f1 - f2
+    assert_equal 1, f.numerator
+    assert_equal 6, f.denominator
+  end
+
+  def test_subtract_fraction_and_integer
+    f1 = Fraction.new(1,2)
+    i = 1
+    f = f1 - i
+    assert_equal -1, f.numerator
+    assert_equal 2, f.denominator
+  end
+
+  def test_multiplication_fraction
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    f = f1 * f2
+    assert_equal 1, f.numerator
+    assert_equal 6, f.denominator
+  end
+
+  def test_multiplication_fraction_with_reduce
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(2,3)
+    f = f1 * f2
+    assert_equal 1, f.numerator
+    assert_equal 3, f.denominator
+  end
+
+  def test_inverse
+    f = Fraction.new(2,3)
+    f.inverse
+    assert_equal 3, f.numerator
+    assert_equal 2, f.denominator
+  end
+
+  def test_divede
+    f1 = Fraction.new(1,4)
+    f2 = Fraction.new(2,3)
+    f = f1 / f2
+    assert_equal 3, f.numerator
+    assert_equal 8, f.denominator
+  end
+   
+  def test_compare_same
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,2)
+    assert f1 == f2
+  end
+
+  def test_compare_diffrent
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,4)
+    refute f1 == f2
+  end
+
+  def test_complete
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    f1.numerator, f1.denominator, f2 = Fraction.complete(f1, f2)
+    assert_equal 3, f1.numerator
+    assert_equal 6, f1.denominator
+    assert_equal 2, f2.numerator
+    assert_equal 6, f2.denominator
+  end
+
+  def test_compare_true
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    assert f1 > f2
+    assert f2 < f1
+  end
+
+  def test_compare_false
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    refute f1 < f2
+    refute f2 > f1
+  end
+
+  def test_compare_equal
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    refute f1 <= f2
+    refute f2 >= f1
+    assert f1 <= f1
+    assert f2 >= f2
+  end
+
+  def test_ufo
+    f1 = Fraction.new(1,2)
+    f2 = Fraction.new(1,3)
+    refute (f1 <=> f2).negative?
+    assert (f2 <=> f1).negative?
+    assert (f1 <=> f1).zero?
+    assert (f1 <=> 1).negative?
+  end
+
+  def test_power
+    f1 = Fraction.new(2,3)
+    f1 **= 3
+    assert 8, f1.numerator
+    assert 3, f1.denominator
+  end
+
+
+
 end
 
