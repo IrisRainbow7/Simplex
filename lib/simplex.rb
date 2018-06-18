1a99b399b7d83b92fb2991e3318381d3c7448241
Iris <a17.bnxk@g.chuo-u.ac.jp>
2018-06-18 22:15:49 +0900

    ピボット演算実装(swap)

--- lib/simplex.rb
+++ lib/simplex.rb
@@ -2,23 +2,91 @@ require './lib/fraction'
 
 
 class Simplex
-  attr_accessor :table
+  attr_accessor :table, :n, :m, :size_x, :size_y
 
-def initialize(table)
-  @table = table
+  def initialize(table)
+    @table = table
+    @table_size_y = table.size
+    @table_size_x = table[0].size
+    @size_y = @table_size_y - 2
+    @size_x = @table_size_x - @size_y - 2
+    @n = 0
+    @m = 0
+  end
+
+  def solve()
+
+  end
+
+  def phase1()
+
+  end
+
+  def phase2()
+
+  end
+
+  def select_max_from_all
+    self.n = self.table[1][2..-1].index(self.table[1][2..-1].max)+2
+  end
+
+  def select_min_ratio_from_base
+    self.m = []
+    2.upto(self.table.size-1) do |i|
+      self.m << self.table[i][1].to_f/self.table[i][self.n].to_f
+    end
+    self.m = self.m.index(self.m.min)+2
+  end
+
+  def base_table
+    tmp_table = Array.new(@table_size_y).map{ Array.new(@table_size_x,0) }
+    tmp_table[0] = self.table[0]
+    tmp_table[1][0] = 'z'
+    2.upto(@table_size_y-1) do |i|
+      tmp_table[i][0] = self.table[i][0]
+    end
+    tmp_table
+  end
+
+  def swap
+    tmp_table = self.base_table
+    replacement = self.table[0].index(self.table[self.m][0])
+    one_positon = -1
+    2.upto(@table_size_y-1) do |i|
+      tmp_table[i][self.n] = self.table[i][replacement]
+      if self.table[i][replacement] == 1
+        one_positon = i
+      end
+    end
+    if one_positon == -1
+      raise 'something is broken'
+    end
+    ratio = self.table[one_positon][self.n]
+    1.upto(@table_size_x-1) do |j|
+      tmp_table[one_positon][j] =  Fraction.new(self.table[one_positon][j],ratio)
+    end
+    1.upto(@table_size_y-1) do |i|
+      next if i == one_positon
+      1.upto(@table_size_x-1) do |j|
+        tmp_table[i][j] = self.table[i][j].to_fr - tmp_table[one_positon][j].to_fr * self.table[i][self.n].to_fr
+      end
+    end
+    tmp_table[self.m][0] = self.table[0][self.n]
+    self.table = tmp_table
+  end
+
+
 end
 
-def solve()
-
-end
-
-def phase1()
-
-end
-
-def phase2()
-
-end
-
-
+if __FILE__ == $0
+  input_data = [['基底', '値', 'x1', 'x2', 'y1', 'y2', 'y3'],
+                ['z', 0, 4, 3, 0, 0, 0],
+                ['y1', 2, 1, 2, 1, 0, 0],
+                ['y2', 19, 12, 18, 0, 1, 0],
+                ['y3',7, 6, 4, 0, 0, 1]]
+  s = Simplex.new(input_data)
+  s.select_max_from_all
+  s.select_min_ratio_from_base
+  s.swap
+  pp s.table.map{|row| row.map(&:to_s)}
 end
--- test/simplex_test.rb
+++ test/simplex_test.rb
@@ -3,27 +3,64 @@ require './lib/simplex'
 
 class Simplextest < Minitest::Test
 
-def test_simplex_without_artificial
-  imput_data = [[0, 4, 3, 0, 0, 0],
-                [2, 1, 2, 1, 0, 0],
-                [19, 12, 18, 0, 1, 0],
-                [7, 6, 4, 0, 0, 1]]
+  def setup
+    @imput_data = [['基底', '値', 'x1', 'x2', 'y1', 'y2', 'y3'],
+                   ['z', 0, 4, 3, 0, 0, 0],
+                   ['y1', 2, 1, 2, 1, 0, 0],
+                   ['y2', 19, 12, 18, 0, 1, 0],
+                   ['y3',7, 6, 4, 0, 0, 1]]
+    @expected_data = [['基底', '値', 'x1', 'x2', 'y1', 'y2', 'y3'],
+                      ['z', Fraction.new(-29,6), 0, 0, 0, Fraction.new(-1,30), Fraction.new(-11,15)],
+                      ['y1', Fraction.new(1,6), 0, 0, 1, Fraction.new(-2,15), Fraction.new(-13,30)],
+                      ['x2', Fraction.new(1,2), 0, 1, 0, Fraction.new(1,10), Fraction.new(1,5)],
+                      ['x1', Fraction.new(5,6), 1, 0, 0, Fraction.new(-1,15), Fraction.new(-9,15)]]
+    @s = Simplex.new(@imput_data)
+  end
 
-  s = Simplex.new(imput_data)
-  s.solve
-
-  expected_data = [[Fraction.new(-29,6), 0, 0, 0, Fraction.new(-1,30), Fraction.new(-11,15)],
-                   [Fraction.new(1,6), 0, 0, 1, Fraction.new(-2,15), Fraction.new(-13,30)],
-                   [Fraction.new(1,2), 0, 1, 0, Fraction.new(1,10), Fraction.new(1,5)],
-                   [Fraction.new(5,6), 1, 0, 0, Fraction.new(-1,15), Fraction.new(-9,15)]]
-
-  assert_equal expected_data, s.table
-end
+  def test_simplex_without_artificial
+    @s.solve
+    assert_equal @expected_data, @s.table
+  end
 
   def test_fraction
     assert Fraction.new(1,1)
   end
 
+  def test_select_max_from_all
+    n = @s.select_max_from_all
+    assert_equal 2, n
+  end
 
+  def test_select_min_ratio_from_base
+    @s.n = 2 
+    m = @s.select_min_ratio_from_base
+    assert_equal 4, m
+  end
+
+  def test_base_table
+    expected_data = [['基底', '値', 'x1', 'x2', 'y1', 'y2', 'y3'],
+                     ['z', 0, 0, 0, 0, 0, 0],
+                     ['y1', 0, 0, 0, 0, 0, 0],
+                     ['y2', 0, 0, 0, 0, 0, 0],
+                     ['y3', 0, 0, 0, 0, 0, 0]]
+    assert_equal expected_data, @s.base_table
+  end
+
+  def test_size
+    assert_equal 2, @s.size_x
+    assert_equal 3, @s.size_y
+  end
+
+  def test_swap
+    @expected_data = [['基底', '値', 'x1', 'x2', 'y1', 'y2', 'y3'],
+                     ['z', Fraction.new(-14,3), 0, Fraction.new(1,3), 0, 0, Fraction.new(-2,3)],
+                     ['y1', Fraction.new(5,6), 0, Fraction.new(4,3), Fraction.new(1,1), 0, Fraction.new(-1,6)],
+                     ['y2', Fraction.new(5,1), 0, Fraction.new(10,1), 0, Fraction.new(1,1), Fraction.new(-2,1)],
+                     ['x1', Fraction.new(7,6), Fraction.new(1,1), Fraction.new(2,3), Fraction.new(0,0), Fraction.new(0,0), Fraction.new(1,6)]]
+    @s.select_max_from_all
+    @s.select_min_ratio_from_base
+    @s.swap
+    assert_equal @expected_data, @s.table
+  end
 
 end
