2f611148ef2ffce68375fe62111508a8ecdf0fec
Iris <a17.bnxk@g.chuo-u.ac.jp>
2018-06-30 22:55:41 +0900

    2段階法初期実装

--- lib/simplex.rb
+++ lib/simplex.rb
@@ -18,17 +18,21 @@ class Simplex
 
   end
 
-  def phase1()
-
-  end
-
-  def phase2
+  def minimize
     until self.table[1][2..-1].map(&:to_fr).all?{|element| element.zero? or element.negative?}
       self.select_max_from_all
       self.select_min_ratio_from_base
       self.swap
       #pp self.table.map{|row| row.map(&:to_s)}
     end
+  end
+
+  def phase1
+    minimize
+  end
+
+  def phase2
+    minimize
   end
 
   def select_max_from_all
@@ -38,7 +42,7 @@ class Simplex
   def select_min_ratio_from_base
     self.m = []
     2.upto(self.table.size-1) do |i|
-      if self.table[i][0] == 'w'
+      if self.table[i][0] == 'w' or self.table[i][0] == 'z'
         self.m << Float::INFINITY
       elsif self.table[i][self.n] > 0
         self.m << self.table[i][1].to_f/self.table[i][self.n].to_f
@@ -52,7 +56,7 @@ class Simplex
   def base_table
     tmp_table = Array.new(@table_size_y).map{ Array.new(@table_size_x,0) }
     tmp_table[0] = self.table[0]
-    tmp_table[1][0] = 'z'
+    tmp_table[1][0] = self.table[1][0]
     2.upto(@table_size_y-1) do |i|
       tmp_table[i][0] = self.table[i][0]
     end
@@ -113,7 +117,13 @@ if __FILE__ == $0
                  ['x1', Fraction.new(73,5), 1, Fraction.new(4,5), 0, 0, Fraction.new(-1,5)],
                  ['x3', Fraction.new(46,5), 0, Fraction.new(3,5), 1, 0, Fraction.new(-2,5)],
                  ['x4',Fraction.new(12,5), 0, Fraction.new(1,5), 0, 1, Fraction.new(-4,5)]]
-s = Simplex.new(input_data4.map {|row| row.map{|element| element.instance_of?(Integer) ? element.to_fr : element }})
+  input_data5 = [['基底', '値', 'x1', 'x2','x3', 'x4', 'y2', 'y3'],
+                 ['w', 20, 4, 4, 0, -1, 0, 0],
+                 ['x3', 20, 2, 5, 1, 0, 0, 0],
+                 ['y2', 14, 3, 2, 0, -1, 1, 0],
+                 ['y3', 6, 1, 2, 0, 0, 0, 1],
+                 ['z', 0, 2, 3, 0, 0, 0, 0]]
+  s = Simplex.new(input_data5.map {|row| row.map{|element| element.instance_of?(Integer) ? element.to_fr : element }})
   s.phase2
-  pp s.table.map{|row| row.map(&:to_s)}
+#  pp s.table.map{|row| row.map(&:to_s)}
 end
